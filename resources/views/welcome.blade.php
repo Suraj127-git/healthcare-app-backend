9afc358ec1aa0b4df8d0b0c2d9e3945a0a4e7d82
Suraj127-git <shetty44444@gmail.com>
2025-01-05 03:06:46 +0530

    changing again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Testing New</title>
+        <title>Laravel Testing New new</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
